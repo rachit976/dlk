bb53f8d2d72c20ead8e3e647177fd4576375eded
rachit976 <mukeshsoni38@gmail.com>
2025-04-27 20:03:06 +0530

    Add files via upload

---
+++ s1 ={2 3 1 }.py
@@ -0,0 +1,11 @@
+s1 ={2 3 1 }
+s1 ={2 3 1 }
+s1 ={2 3 1 }
+list1 [10 20 30 ]
+list2[10020300400]
+
+stocks =['2175,1127 27']
+prices[133 5 354]
+new_dict{stocks:prices for stocks}
+prices in zip (stocks prices)
+ print(\n format(new_dict))
